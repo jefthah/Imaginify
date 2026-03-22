e5821b7dd0f7bbe2f5f4150fd9f69d6f1acae7f3
jefthah <jefta.supraja@gmail.com>
2026-03-22 18:29:19 +0700

    implement sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,11 @@
+import SIdebar from "@/components/shared/SIdebar";
 import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="root">
+        <SIdebar/>
+        {/* mobile nav */}
       <div className="root-container">
         <div className="wrapper">{children}</div>
       </div>
