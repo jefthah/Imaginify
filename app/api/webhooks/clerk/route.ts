592ae4b690060453d8571a108966703156e7536a
jefthah <jefta.supraja@gmail.com>
2026-03-24 21:22:04 +0700

    adjust

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,4 +1,3 @@
-/* eslint-disable camelcase */
 import { clerkClient } from "@clerk/nextjs/server";
 import { WebhookEvent } from "@clerk/nextjs/server";
 import { headers } from "next/headers";
@@ -65,8 +64,8 @@ export async function POST(req: Request) {
       clerkId: id,
       email: email_addresses[0].email_address,
       username: username!,
-      firstName: first_name,
-      lastName: last_name,
+      firstName: first_name || "",
+      lastName: last_name || "",
       photo: image_url,
     };
 
@@ -90,8 +89,8 @@ export async function POST(req: Request) {
     const { id, image_url, first_name, last_name, username } = evt.data;
 
     const user = {
-      firstName: first_name,
-      lastName: last_name,
+      firstName: first_name || "",
+      lastName: last_name || "",
       username: username!,
       photo: image_url,
     };
