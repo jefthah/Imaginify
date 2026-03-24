c846d20e271a477bb6b94a5629707dca764bede7
jefthah <jefta.supraja@gmail.com>
2026-03-24 13:05:22 +0700

    add mobile view sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,12 @@
+import MobileNav from "@/components/shared/MobileNav";
 import SIdebar from "@/components/shared/SIdebar";
 import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="root">
-        <SIdebar/>
-        {/* mobile nav */}
+      <SIdebar />
+      <MobileNav />
       <div className="root-container">
         <div className="wrapper">{children}</div>
       </div>
